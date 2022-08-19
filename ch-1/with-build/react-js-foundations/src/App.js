827824baf0d9c070b7222d6b3999de62d14fa2ca
Daevah Taylaran <daevtayl@gmail.com>
2022-08-19 14:13:25 -0700

    organized folders

---
+++ ch-1/with-build/react-js-foundations/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  const [personName, setPersonName] = React.useState('');
+
+  return (
+    <div className="App">
+        <h1>Hello {personName}</h1>
+        <input type="text" placeholder="type your name here" onChange={(e) => setPersonName(e.target.value)}/>
+    </div>
+  )
+}
+
+export default App;
